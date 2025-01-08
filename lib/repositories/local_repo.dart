96278e583e7678723977df719945e1f08974781f
kuper0201 <kuper171@gmail.com>
2025-01-08 14:34:12 +0900

    more clean

--- lib/repositories/local_repo.dart
+++ lib/repositories/local_repo.dart
@@ -3,18 +3,13 @@ import 'package:io/io.dart';
 
 class LocalRepo {
   Future<List<FileSystemEntity>> fetchEntries(String path) async {
-    List<FileSystemEntity> entries;
-    
     try {
       final dir = Directory(path);
-      // entries = await dir.list().toList();
-      entries = dir.listSync().toList();
+      return dir.listSync().toList();
     } catch (e) {
       print('fetch entry error: $e');
       rethrow;
     }
-
-    return entries;
   }
 
   void newDirectory(String totalPath) {
